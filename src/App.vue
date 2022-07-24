bb85796dadafae85ace01821bf103bcd06acebe3
srabon0 <srabonemam4@gmail.com>
2022-07-24 21:40:08 +0600

    first-vue-explore

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">EventList</router-link> |
+    <router-link to="/About">About</router-link>
   </nav>
   <router-view/>
 </template>
